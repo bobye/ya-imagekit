e3fae4f3f464b60352b6be21b9b906d6a8534b8f
Jianbo Ye <jxy198@ist.psu.edu>
2015-01-23 10:12:39 -0500

    small updates

--- synthetic/westlake2-script.sh
+++ synthetic/westlake2-script.sh
@@ -1,8 +1,9 @@
 path=/gpfs/home/jxy198/group/jianbo/paintings/Westlake
+feat=supgrad_v0
 
 for filename in `ls -1 $path/westlake_by_artists/*/*.jpg`
 do
-	k=`echo $filename | sed 's/westlake_by_artists/signature/g' | sed 's/.jpg/.supgrad.mat/g'`
+	k=`echo $filename | sed "s/westlake_by_artists/$feat/g" | sed 's/.jpg/.supgrad.mat/g'`
 	if [ ! -f "$k" ]
 	then
 		qsub -v ifile="$filename",ofile="$k" job_submit.sh	
